2388b38e9b2982875513e83d0db9d56d33c3b827
Jeremy Muhia <jmuhia80@gmail.com>
2024-07-02 10:42:18 -0400

    Write simple helloworld repository test

--- api/internal/helloworld/repository_test.go
+++ api/internal/helloworld/repository_test.go
@@ -1,1 +1,32 @@
 package helloworld
+
+import (
+	"context"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/protobuf/types/known/emptypb"
+	"testing"
+)
+
+func TestRepository_GetHelloWorld(t *testing.T) {
+	tests := map[string]struct {
+		wantCode   string
+		wantOutput string
+	}{
+		"valid": {
+			wantCode:   codes.OK.String(),
+			wantOutput: "Hello World",
+		},
+	}
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			repo := NewRepository()
+			actual, err := repo.GetHelloWorld(context.Background(), &emptypb.Empty{})
+			if err != nil {
+				t.Errorf("GetHelloWorld got unexpected error: %v", err)
+			}
+			if tc.wantCode != actual.Status {
+				t.Errorf("want %s, got %s", tc.wantCode, actual.Status)
+			}
+		})
+	}
+}
