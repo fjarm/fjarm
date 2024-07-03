33ee9f8ffc8f6f1981dba0dbd4134e1e4e28a249
Jeremy Muhia <jmuhia80@gmail.com>
2024-07-03 10:38:41 -0400

    Serve helloworld gRPC server

--- api/cmd/helloworld/main.go
+++ api/cmd/helloworld/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"github.com/fjarm/fjarm/api/internal/helloworld"
+	pb "github.com/fjarm/fjarm/api/pkg/helloworld/v1"
+	"google.golang.org/grpc"
+	"log/slog"
+	"net"
+	"os"
+)
+
+const address = "0.0.0.0:8080"
+
+func serve(lis net.Listener) error {
+	srv := grpc.NewServer()
+	pb.RegisterHelloWorldServiceServer(srv, helloworld.NewService())
+	slog.Info("starting server", "addr", address)
+
+	if err := srv.Serve(lis); err != nil {
+		return err
+	}
+	return nil
+}
 
 func main() {
-	fmt.Println("Hello world")
+	lis, err := net.Listen("tcp", address)
+	if err != nil {
+		slog.Error("failed to listen", "err", err)
+		os.Exit(1)
+	}
+
+	if err = serve(lis); err != nil {
+		slog.Error("failed to serve", "err", err)
+		os.Exit(1)
+	}
 }
