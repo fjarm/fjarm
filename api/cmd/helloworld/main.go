13da4cf07ed15d3c94c8e6b18aea39db7650e745
Jeremy Muhia <jmuhia80@gmail.com>
2024-07-03 10:47:31 -0400

    Read PORT from environment

--- api/cmd/helloworld/main.go
+++ api/cmd/helloworld/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/fjarm/fjarm/api/internal/helloworld"
 	pb "github.com/fjarm/fjarm/api/pkg/helloworld/v1"
 	"google.golang.org/grpc"
@@ -9,12 +10,12 @@ import (
 	"os"
 )
 
-const address = "0.0.0.0:8080"
+const ip = "0.0.0.0"
 
 func serve(lis net.Listener) error {
 	srv := grpc.NewServer()
 	pb.RegisterHelloWorldServiceServer(srv, helloworld.NewService())
-	slog.Info("starting server", "addr", address)
+	slog.Info("starting server", "addr", lis.Addr())
 
 	if err := srv.Serve(lis); err != nil {
 		return err
@@ -23,7 +24,14 @@ func serve(lis net.Listener) error {
 }
 
 func main() {
-	lis, err := net.Listen("tcp", address)
+	port := os.Getenv("PORT")
+	if port == "" {
+		slog.Error("failed to read port from environment")
+		os.Exit(1)
+	}
+
+	addr := fmt.Sprintf("%s:%s", ip, port)
+	lis, err := net.Listen("tcp", addr)
 	if err != nil {
 		slog.Error("failed to listen", "err", err)
 		os.Exit(1)
