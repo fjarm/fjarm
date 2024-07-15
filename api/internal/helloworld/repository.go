3db7b643e068ac68fc3896ac62260b0960aa49ca
Jeremy Muhia <jmuhia80@gmail.com>
2024-07-15 09:36:52 -0400

    Force failure by updating status code returned

--- api/internal/helloworld/repository.go
+++ api/internal/helloworld/repository.go
@@ -9,5 +9,5 @@ import (
 type repository struct{}
 
 func (repo repository) getHelloWorld(_ context.Context) (*pb.GetHelloWorldResponse, error) {
-	return &pb.GetHelloWorldResponse{Status: codes.OK.String(), Output: "Hello World"}, nil
+	return &pb.GetHelloWorldResponse{Status: codes.Unknown.String(), Output: "Hello World"}, nil
 }
