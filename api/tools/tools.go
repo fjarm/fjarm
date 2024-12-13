af30e0252020938e51a92587d8e6343046726df6
Jeremy Muhia <jmuhia80@gmail.com>
2024-12-12 20:28:22 -0500

    Remove unecessary protovalidate-go import in tools (#23)

--- api/tools/tools.go
+++ api/tools/tools.go
@@ -1,6 +1,5 @@
 package tools
 
 import (
-    _ "github.com/bufbuild/protovalidate-go"
-    _ "buf.build/gen/go/bufbuild/protovalidate/protocolbuffers/go/buf/validate"
+	_ "buf.build/gen/go/bufbuild/protovalidate/protocolbuffers/go/buf/validate"
 )
