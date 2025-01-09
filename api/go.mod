0c38962a681c14186ba808956bc4acc0134699a8
Jeremy Muhia <jmuhia80@gmail.com>
2025-01-09 09:44:16 -0500

    Test userservice.v1.FullName proto validation rules in Go (#39)
    * Bump Go version, Go rules version, and Gazelle rules version
    
    * Bump Buf SDK Go module version
    
    * Test userservice.v1.FullName proto message

--- api/go.mod
+++ api/go.mod
@@ -1,15 +1,15 @@
 module github.com/fjarm/fjarm/api
 
-go 1.22.4
+go 1.23.4
 
 require (
-	buf.build/gen/go/bufbuild/protovalidate/protocolbuffers/go v1.36.1-20241127180247-a33202765966.1
+	buf.build/gen/go/bufbuild/protovalidate/protocolbuffers/go v1.36.2-20241127180247-a33202765966.1
 	buf.build/gen/go/fjarm/fjarm/grpc/go v1.5.1-20250108200146-7eb9c574781f.2
-	buf.build/gen/go/fjarm/fjarm/protocolbuffers/go v1.36.1-20250108200146-7eb9c574781f.1
+	buf.build/gen/go/fjarm/fjarm/protocolbuffers/go v1.36.2-20250108221344-e92a7c0d7fec.1
 	github.com/bufbuild/protovalidate-go v0.8.2
 	github.com/google/uuid v1.6.0
 	google.golang.org/grpc v1.65.0
-	google.golang.org/protobuf v1.36.1
+	google.golang.org/protobuf v1.36.2
 )
 
 require (
---
+++ api/pkg/userservice/v1/user_full_name_test.go
@@ -0,0 +1,62 @@
+package v1
+
+import (
+	userservicepb "buf.build/gen/go/fjarm/fjarm/protocolbuffers/go/userservice/v1"
+	"github.com/bufbuild/protovalidate-go"
+	"testing"
+)
+
+func TestUserFullName_Validation(t *testing.T) {
+	validator, err := protovalidate.New(
+		protovalidate.WithDisableLazy(true),
+		protovalidate.WithMessages(
+			&userservicepb.FullName{},
+		),
+	)
+	if err != nil {
+		t.Errorf("failed to initialize validator: %v", err)
+	}
+	tests := map[string]struct {
+		first   string
+		last    string
+		wantErr bool
+	}{
+		"valid_from_example": {
+			first:   "bella",
+			last:    "hadid",
+			wantErr: false,
+		},
+		"valid_first_and_last_name_one_character": {
+			first:   "b",
+			last:    "h",
+			wantErr: false,
+		},
+		"invalid_first_name_empty": {
+			first:   "",
+			last:    "hadid",
+			wantErr: true,
+		},
+		"invalid_last_name_empty": {
+			first:   "bella",
+			last:    "",
+			wantErr: true,
+		},
+		"invalid_first_and_last_name_empty": {
+			first:   "",
+			last:    "",
+			wantErr: true,
+		},
+	}
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			fullName := &userservicepb.FullName{
+				FamilyName: &tc.first,
+				GivenName:  &tc.last,
+			}
+			err = validator.Validate(fullName)
+			if (err != nil) != tc.wantErr {
+				t.Errorf("got error = %v, wantErr = %v, input = %v, %v", err, tc.wantErr, tc.first, tc.last)
+			}
+		})
+	}
+}
