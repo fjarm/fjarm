de792d6858dbdbc6a49bbb0c7ac3f5f05e993756
Jeremy Muhia <jmuhia80@gmail.com>
2025-01-27 20:52:33 -0500

    Set up tests for user handle Proto

---
+++ api/pkg/users/v1/user_handle_test.go
@@ -0,0 +1,51 @@
+package v1
+
+import (
+	userservicepb "buf.build/gen/go/fjarm/fjarm/protocolbuffers/go/fjarm/users/v1"
+	"github.com/bufbuild/protovalidate-go"
+	"testing"
+)
+
+func TestUserHandle_Validation(t *testing.T) {
+	validator, err := protovalidate.New(
+		protovalidate.WithDisableLazy(true),
+		protovalidate.WithMessages(
+			&userservicepb.UserHandle{},
+		),
+	)
+	if err != nil {
+		t.Errorf("failed to initialize validator: %v", err)
+	}
+	tests := map[string]struct {
+		handle  string
+		wantErr bool
+	}{
+		"valid_single_character": {
+			handle:  "a",
+			wantErr: false,
+		},
+		"valid_from_example": {
+			handle:  "therealgleepglop",
+			wantErr: false,
+		},
+		"invalid_includes_space": {
+			handle:  "the real",
+			wantErr: true,
+		},
+		"invalid_incorrect_length": {
+			handle:  "",
+			wantErr: true,
+		},
+	}
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			handle := &userservicepb.UserHandle{
+				Handle: &tc.handle,
+			}
+			err = validator.Validate(handle)
+			if err != nil && !tc.wantErr {
+				t.Errorf("got error = %v, wantErr = %v, input = %v", err, tc.wantErr, tc.handle)
+			}
+		})
+	}
+}
