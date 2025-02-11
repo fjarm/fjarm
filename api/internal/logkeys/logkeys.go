968f59de17885fa4155b69130048113c8c0de9ef
Jeremy Muhia <jmuhia80@gmail.com>
2025-02-10 21:20:38 -0500

    Add timing based log keys

--- api/internal/logkeys/logkeys.go
+++ api/internal/logkeys/logkeys.go
@@ -1,9 +1,11 @@
 package logkeys
 
 const Addr = "addr"
+const Duration = "duration"
 const Err = "error"
 const Raw = "raw"
 const Request = "req"
 const Rpc = "rpc"
 const Service = "service"
+const StartTime = "start"
 const Tag = "tag"
