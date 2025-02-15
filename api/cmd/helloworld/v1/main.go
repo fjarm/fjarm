6e04ab6a1774c0ec8b25e72fc3339226633a5be9
Jeremy Muhia <jmuhia80@gmail.com>
2025-02-15 15:04:31 -0500

    Update helloworld service executable to use ConnectRPC

--- api/cmd/helloworld/v1/main.go
+++ api/cmd/helloworld/v1/main.go
@@ -1,90 +1,84 @@
 package main
 
 import (
-	rpc "buf.build/gen/go/fjarm/fjarm/grpc/go/fjarm/helloworld/v1/helloworldv1grpc"
+	"buf.build/gen/go/fjarm/fjarm/connectrpc/go/fjarm/helloworld/v1/helloworldv1connect"
 	"context"
+	"errors"
 	"fmt"
 	helloworld "github.com/fjarm/fjarm/api/internal/helloworld/v1"
 	"github.com/fjarm/fjarm/api/internal/logkeys"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
 	"log/slog"
 	"net"
+	"net/http"
 	"os"
 	"os/signal"
-	"syscall"
 )
 
 const ip = "[::]"
 
-const serviceName = "helloworld"
+const mainTag = "main"
 
 func main() {
-	// Use flags here instead of environment variable
-	port := os.Getenv("PORT")
-	if port == "" {
-		slog.Error(
-			"failed to read port from environment",
-			slog.String(logkeys.Service, serviceName),
-		)
-		os.Exit(1)
-	}
-
-	ctx := context.Background()
-
-	addr := fmt.Sprintf("%s:%s", ip, port)
-	lis, err := net.Listen("tcp6", addr)
-	if err != nil {
-		slog.ErrorContext(
-			ctx,
-			"failed to create listener",
-			slog.String(logkeys.Service, serviceName),
-			slog.String(logkeys.Addr, addr),
-			slog.String(logkeys.Err, err.Error()),
-		)
-		os.Exit(1)
-	}
-
-	srv := grpc.NewServer()
-	han := helloworld.NewGrpcHandler()
-	if han == nil {
-		slog.ErrorContext(ctx, "failed to create gRPC handler", slog.String(logkeys.Service, serviceName))
-		os.Exit(1)
-	}
-	rpc.RegisterHelloWorldServiceServer(srv, han)
-	reflection.Register(srv)
-
-	closer := func() {
-		slog.InfoContext(ctx, "stopping server", slog.String(logkeys.Service, serviceName))
-		e := lis.Close()
-		if e != nil {
-			slog.ErrorContext(ctx, "failed to close listener", slog.String(logkeys.Service, serviceName))
-		}
-		srv.GracefulStop()
-	}
-	defer closer()
-
-	slog.InfoContext(
-		ctx,
-		"starting server",
-		slog.String(logkeys.Service, serviceName),
-		slog.String(logkeys.Addr, addr),
+	logger := slog.New(
+		slog.NewJSONHandler(os.Stdout, nil),
+	).With(
+		slog.String(logkeys.Service, helloworldv1connect.HelloWorldServiceName),
 	)
 
-	go func() {
-		e := srv.Serve(lis)
-		if e != nil {
-			slog.ErrorContext(
-				ctx,
-				"failed to start serving",
-				slog.String(logkeys.Service, serviceName),
-				slog.String(logkeys.Err, err.Error()),
-			)
-			os.Exit(1)
+	// Handle SIGINT (CTRL+C) gracefully.
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		logger.ErrorContext(ctx, "failed to read port from environment", slog.String(logkeys.Tag, mainTag))
+		os.Exit(1)
+	}
+	addr := fmt.Sprintf("%s:%s", ip, port)
+
+	connectRPCHandler := helloworld.NewConnectRPCHandler(logger)
+	path, handler := helloworldv1connect.NewHelloWorldServiceHandler(connectRPCHandler)
+
+	mux := http.NewServeMux()
+	mux.Handle(path, handler)
+
+	srv := &http.Server{
+		Addr: addr,
+		BaseContext: func(_ net.Listener) context.Context {
+			return ctx
+		},
+		Handler: mux,
+	}
+
+	defer func() {
+		logger.InfoContext(ctx, "shut down server", slog.String(logkeys.Tag, mainTag))
+
+		err := srv.Shutdown(ctx)
+		if err != nil && !errors.Is(err, http.ErrServerClosed) {
+			logger.ErrorContext(ctx, "failed to shut down server", slog.String(logkeys.Tag, mainTag))
 		}
 	}()
 
-	quit := make(chan os.Signal, 1)
-	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
-	<-quit
+	srvErrChan := make(chan error, 1)
+	go func() {
+		logger.InfoContext(ctx, "started server", slog.String(logkeys.Tag, mainTag))
+		srvErrChan <- srv.ListenAndServe()
+	}()
+
+	// Wait for interruption.
+	select {
+	case err := <-srvErrChan:
+		// Error when starting HTTP server.
+		logger.ErrorContext(
+			ctx,
+			"stopping server after error",
+			slog.String(logkeys.Tag, mainTag),
+			slog.Any(logkeys.Err, err),
+		)
+		return
+	case <-ctx.Done():
+		// Wait for first CTRL+C.
+		// Stop receiving signal notifications as soon as possible.
+		stop()
+	}
 }
