1359935e2b9eb1564b8a8bb88ccf98b347e4e8bd
Jeremy Muhia <jmuhia80@gmail.com>
2025-02-16 21:27:54 -0500

    Set up user storage entity and etag generation

--- api/internal/users/v1/user.go
+++ api/internal/users/v1/user.go
@@ -3,39 +3,85 @@ package v1
 import (
 	userspb "buf.build/gen/go/fjarm/fjarm/protocolbuffers/go/fjarm/users/v1"
 	"connectrpc.com/connect"
+	"crypto/sha256"
+	"encoding/base64"
 	"fmt"
 	"time"
 )
 
 // user defines the entity as represented in persisted storage like a SQL database. This type is internal to the users
 // service. External services get back a `fjarm.users.v1.User` Protobuf message instead.
+//
+// Note  that pointer fields are optional. Thus, pointers are used to distinguish between a valid value, an explicit
+// null (nil pointer), or a zero value.
 type user struct {
 	UserID       string    `db:"user_id"`
 	GivenName    string    `db:"given_name"`
 	FamilyName   string    `db:"family_name"`
-	UserHandle   string    `db:"handle"`
+	Handle       string    `db:"handle"`
 	EmailAddress string    `db:"email_address"`
-	Avatar       string    `db:"avatar"`
 	Password     string    `db:"password"`
 	Salt         string    `db:"salt"`
-	LastUpdated  time.Time `db:"last_updated"`
+	Avatar       *string   `db:"avatar"`       // Optional field/column. Can be nil/NULL.
+	LastUpdated  time.Time `db:"last_updated"` // Used primarily for ETag calculation.
 	CreatedAt    time.Time `db:"created_at"`
+}
+
+func (usr *user) calculateETag() string {
+	hasher := sha256.New()
+
+	// Note: We're explicitly ordering fields to ensure consistency
+	hasher.Write([]byte(usr.UserID))
+	hasher.Write([]byte(usr.GivenName))
+	hasher.Write([]byte(usr.FamilyName))
+	hasher.Write([]byte(usr.Handle))
+	hasher.Write([]byte(usr.EmailAddress))
+	if usr.Avatar != nil {
+		hasher.Write([]byte(*usr.Avatar))
+	}
+	hasher.Write([]byte(usr.LastUpdated.UTC().Format(time.RFC3339Nano)))
+	hasher.Write([]byte(usr.CreatedAt.UTC().Format(time.RFC3339Nano)))
+
+	// Convert hash to base64 for a shorter string representation
+	// Use RawURLEncoding to ensure URL-safe characters
+	return base64.RawURLEncoding.EncodeToString(hasher.Sum(nil))
+}
+
+func storageUserToWireUser(su *user) (*userspb.User, error) {
+	if su == nil {
+		return nil, connect.NewError(
+			connect.CodeInvalidArgument,
+			fmt.Errorf("cannot convert nil user to user message. %w", ErrInvalidArgument),
+		)
+	}
+	usr := userspb.User{
+		UserId: &userspb.UserId{UserId: &su.UserID},
+		FullName: &userspb.UserFullName{
+			FamilyName: &su.FamilyName,
+			GivenName:  &su.GivenName,
+		},
+		Handle:       &userspb.UserHandle{Handle: &su.Handle},
+		EmailAddress: &userspb.UserEmailAddress{EmailAddress: &su.EmailAddress},
+		Avatar:       &userspb.UserAvatar{Avatar: su.Avatar},
+	}
+	return &usr, nil
 }
 
 func wireUserToStorageUser(msg *userspb.User) (*user, error) {
 	if msg == nil {
 		return nil, connect.NewError(
 			connect.CodeInvalidArgument,
-			fmt.Errorf("cannot convert nil message to user. %w", ErrInvalidArgument),
+			fmt.Errorf("cannot convert nil user message to user. %w", ErrInvalidArgument),
 		)
 	}
+	avi := msg.GetAvatar().GetAvatar()
 	usr := user{
 		UserID:       msg.GetUserId().GetUserId(),
 		GivenName:    msg.GetFullName().GetGivenName(),
 		FamilyName:   msg.GetFullName().GetFamilyName(),
-		UserHandle:   msg.GetHandle().GetHandle(),
+		Handle:       msg.GetHandle().GetHandle(),
 		EmailAddress: msg.GetEmailAddress().GetEmailAddress(),
-		Avatar:       msg.GetAvatar().GetAvatar(),
+		Avatar:       &avi,
 	}
 	return &usr, nil
 }
--- api/internal/users/v1/user_test.go
+++ api/internal/users/v1/user_test.go
@@ -3,11 +3,68 @@ package v1
 import (
 	"errors"
 	"testing"
+	"time"
 )
 
 func TestWireUserToStorageUser_ErrorWrapping(t *testing.T) {
 	_, err := wireUserToStorageUser(nil)
 	if !errors.Is(err, ErrInvalidArgument) {
 		t.Errorf("error is not wrapped correctly")
+	}
+}
+
+func TestStorageUserToWireUser_ErrorWrapping(t *testing.T) {
+	_, err := storageUserToWireUser(nil)
+	if !errors.Is(err, ErrInvalidArgument) {
+		t.Errorf("error is not wrapped correctly")
+	}
+}
+
+func TestCalculateETag(t *testing.T) {
+	tests := map[string]struct {
+		u1    user
+		u2    user
+		equal bool
+	}{
+		"valid_two_empty_users": {
+			u1:    user{},
+			u2:    user{},
+			equal: true,
+		},
+		"valid_two_non_empty_users": {
+			u1: user{
+				UserID:      "abc123",
+				LastUpdated: time.Date(2024, time.December, 12, 12, 12, 12, 12, time.Local),
+				CreatedAt:   time.Date(2024, time.December, 12, 12, 12, 7, 12, time.Local),
+			},
+			u2: user{
+				UserID:      "abc123",
+				LastUpdated: time.Date(2024, time.December, 12, 12, 12, 12, 12, time.Local),
+				CreatedAt:   time.Date(2024, time.December, 12, 12, 12, 7, 12, time.Local),
+			},
+			equal: true,
+		},
+		"invalid_two_non_empty_users": {
+			u1: user{
+				UserID:      "abc123",
+				LastUpdated: time.Now(),
+				CreatedAt:   time.Now(),
+			},
+			u2: user{
+				UserID:      "abc123",
+				LastUpdated: time.Now(),
+				CreatedAt:   time.Now(),
+			},
+			equal: false,
+		},
+	}
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			e1 := tc.u1.calculateETag()
+			e2 := tc.u2.calculateETag()
+			if e1 != e2 && tc.equal {
+				t.Errorf("unexpected two unequal users")
+			}
+		})
 	}
 }
