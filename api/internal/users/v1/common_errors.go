6f7122c2dfd1aeb0090467e3e86ac95c2a4ab268
Jeremy Muhia <jmuhia80@gmail.com>
2025-02-18 10:21:49 -0500

    Introduce already exists error

--- api/internal/users/v1/common_errors.go
+++ api/internal/users/v1/common_errors.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 )
 
+// ErrAlreadyExists is returned when a client tries to create a duplicate resource, in this case a user entity.
+var ErrAlreadyExists = fmt.Errorf("resource already exists")
+
 // ErrUnimplemented is returned when a service method is called before it's been implemented.
 var ErrUnimplemented = fmt.Errorf("method is not implemented")
 
