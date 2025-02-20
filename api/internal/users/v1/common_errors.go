cf9147ffcee517ed72898322c4168196b9b3268e
Jeremy Muhia <jmuhia80@gmail.com>
2025-02-20 10:54:03 -0500

    Create new authentication issue error message wrapper

--- api/internal/users/v1/common_errors.go
+++ api/internal/users/v1/common_errors.go
@@ -7,6 +7,11 @@ import (
 // ErrAlreadyExists is returned when a client tries to create a duplicate resource, in this case a user entity.
 var ErrAlreadyExists = fmt.Errorf("resource already exists")
 
+// ErrAuthenticationIssue is returned when a client fails to authenticate a user. This can happen at user creation when
+// attempting to hash the supplied credentials. It can also fail at user update or deletion when the supplied
+// credentials do not match the stored credentials.
+var ErrAuthenticationIssue = fmt.Errorf("authentication issue")
+
 // ErrUnimplemented is returned when a service method is called before it's been implemented.
 var ErrUnimplemented = fmt.Errorf("method is not implemented")
 
