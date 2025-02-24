0ec31132fc1c6fa951d3355344d8c0735820d7c6
Jeremy Muhia <jmuhia80@gmail.com>
2025-02-24 15:42:40 -0500

    Set up user property validation tests

--- api/pkg/fjarm/users/v1/users.go
+++ api/pkg/fjarm/users/v1/users.go
@@ -1,1 +1,38 @@
 package v1
+
+import (
+	userspb "buf.build/gen/go/fjarm/fjarm/protocolbuffers/go/fjarm/users/v1"
+	"context"
+	"github.com/bufbuild/protovalidate-go"
+)
+
+func ValidateUserHandle(_ context.Context, handle *userspb.UserHandle) error {
+	if handle == nil {
+		return ErrValidationError
+	}
+	if !handle.HasHandle() {
+		return ErrValidationError
+	}
+	return protovalidate.Validate(handle)
+
+}
+
+func ValidateUserID(_ context.Context, id *userspb.UserId) error {
+	if id == nil {
+		return ErrValidationError
+	}
+	if !id.HasUserId() {
+		return ErrValidationError
+	}
+	return protovalidate.Validate(id)
+}
+
+func ValidateUserPassword(_ context.Context, pwd *userspb.UserPassword) error {
+	if pwd == nil {
+		return ErrValidationError
+	}
+	if !pwd.HasPassword() {
+		return ErrValidationError
+	}
+	return protovalidate.Validate(pwd)
+}
