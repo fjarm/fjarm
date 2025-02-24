5f86a11365aa83819afd5db7d5395ecc2256fdee
Jeremy Muhia <jmuhia80@gmail.com>
2025-02-24 11:02:21 -0500

    Set up common validation error in public users package

--- api/pkg/fjarm/users/v1/common_errors.go
+++ api/pkg/fjarm/users/v1/common_errors.go
@@ -2,5 +2,5 @@ package v1
 
 import "fmt"
 
-// ErrInputError is returned when invalid input is submitted for message validation.
-var ErrInputError = fmt.Errorf("invalid input used for message validation")
+// ErrValidationError is returned when invalid input is submitted for message validation.
+var ErrValidationError = fmt.Errorf("failed to validate message")
