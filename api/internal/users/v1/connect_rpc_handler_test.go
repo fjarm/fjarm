a582698a543ccdb367f8950e2b3a27501840aa54
Jeremy Muhia <jmuhia80@gmail.com>
2025-02-25 20:55:07 -0500

    Test CreateUser handler method

--- api/internal/users/v1/connect_rpc_handler_test.go
+++ api/internal/users/v1/connect_rpc_handler_test.go
@@ -1,6 +1,58 @@
 package v1
 
-import "testing"
+import (
+	"buf.build/gen/go/fjarm/fjarm/connectrpc/go/fjarm/users/v1/usersv1connect"
+	userspb "buf.build/gen/go/fjarm/fjarm/protocolbuffers/go/fjarm/users/v1"
+	"connectrpc.com/connect"
+	"context"
+	"errors"
+	"io"
+	"log/slog"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+)
 
-func TestConnectRPCHandler_CreateUser(t *testing.T) {
+var srv *httptest.Server = nil
+
+func TestMain(m *testing.M) {
+	logger := slog.New(slog.NewTextHandler(io.Discard, nil))
+
+	connectRPCHandler := NewConnectRPCHandler(logger)
+	path, handler := usersv1connect.NewUserServiceHandler(connectRPCHandler)
+
+	mux := http.NewServeMux()
+	mux.Handle(path, handler)
+
+	srv = httptest.NewServer(mux)
+	defer srv.Close()
+
+	os.Exit(m.Run())
+}
+
+func TestConnectRPCHandler_CreateUser_gRPCClient(t *testing.T) {
+	tests := map[string]struct {
+		reqs []*userspb.CreateUserRequest
+		errs []bool
+		kind []error
+	}{}
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			client := usersv1connect.NewUserServiceClient(http.DefaultClient, srv.URL, connect.WithGRPC())
+
+			for index, req := range tc.reqs {
+				_, err := client.CreateUser(context.Background(), connect.NewRequest(req))
+				if err != nil && !tc.errs[index] {
+					t.Errorf("CreateUser got an unexpected error: %v", err)
+				}
+				if err == nil && tc.errs[index] {
+					t.Errorf("CreateUser expected an error but got nil")
+				}
+				if !errors.Is(err, tc.kind[index]) {
+					t.Errorf("CreateUser got an unexpected error kind: %v", err)
+				}
+			}
+		})
+	}
 }
