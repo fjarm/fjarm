a37a75572d4cfe6c091e53baece4f79fb312095d
Jeremy Muhia <jmuhia80@gmail.com>
2025-03-23 15:38:54 -0400

    Introduce lock releasing error type

--- api/internal/cache/cache_errors.go
+++ api/internal/cache/cache_errors.go
@@ -13,5 +13,9 @@ var ErrInvalidExpiration = fmt.Errorf("invalid expiration")
 // ErrInvalidKey is returned when a key is invalid - i.e. empty strings or whitespace only strings.
 var ErrInvalidKey = fmt.Errorf("invalid key")
 
+// ErrLockReleaseFailed is returned when a process attempts to delete a key that does not belong to it. This indicates
+// that either another process owns the key/lock or that the key/lock has already been deleted.
+var ErrLockReleaseFailed = fmt.Errorf("failed to release lock")
+
 // ErrKeyExists is returned when a key already exists in the cache.
 var ErrKeyExists = fmt.Errorf("key exists")
