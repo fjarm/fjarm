b6100e87417c11576ceb0efbb9634a44b70f09e2
Jeremy Muhia <jmuhia80@gmail.com>
2025-04-16 09:40:54 -0400

    Set up empty redis server starter struct

--- api/internal/redis/v1/pkg/startup/redis_startup.go
+++ api/internal/redis/v1/pkg/startup/redis_startup.go
@@ -2,3 +2,21 @@ package startup
 
 // TODO(2025-03-15): Create Redis admin client.
 // TODO(2025-03-15): Use Redis admin client to setup ACLs and TLS.
+
+type redisServerStarter struct{}
+
+func (rs *redisServerStarter) createConfigFile() error {
+	return ErrUnimplemented
+}
+
+func (rs *redisServerStarter) fetchCertificates() error {
+	return ErrUnimplemented
+}
+
+func (rs *redisServerStarter) saveCertificates() error {
+	return ErrUnimplemented
+}
+
+func newRedisServerStarter() *redisServerStarter {
+	return &redisServerStarter{}
+}
