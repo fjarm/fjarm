e3fa62acdc327bfadbaef289bf5d0b2fff55dda4
Jeremy Muhia <jmuhia80@gmail.com>
2025-05-17 16:59:53 -0400

    Address macOS specific open command comment

--- tools/bazel_generate_test_coverage_report.sh
+++ tools/bazel_generate_test_coverage_report.sh
@@ -2,4 +2,14 @@
 
 bazel coverage --combined_report=lcov //...
 genhtml --branch-coverage --output genhtml "$(bazel info output_path)/_coverage/_coverage_report.dat"
-open genhtml/index.html
+
+if command -v xdg-open > /dev/null; then
+    xdg-open genhtml/index.html
+elif command -v open > /dev/null; then
+    open genhtml/index.html
+elif [ -n "$BROWSER" ]; then
+   $BROWSER genhtml/index.html
+else
+   echo "Error: Could not detect a method to open the report. Please open genhtml/index.html manually."
+   exit 1
+fi
