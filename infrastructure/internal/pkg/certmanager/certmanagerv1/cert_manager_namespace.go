01772b6f0f87c30c166f4caad36d22fa29bf9b89
Jeremy Muhia <jmuhia80@gmail.com>
2025-07-06 14:10:44 -0400

    Create cert-manager namespace

---
+++ infrastructure/internal/pkg/certmanager/certmanagerv1/cert_manager_namespace.go
@@ -0,0 +1,37 @@
+package certmanagerv1
+
+import (
+	corev1 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/core/v1"
+	metav1 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/meta/v1"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
+
+const (
+	chartNamespace = "cert-manager"
+)
+
+// deployCertManagerNamespace creates a Namespace for cert-manager to be installed in.
+func deployCertManagerNamespace(ctx *pulumi.Context) (*corev1.Namespace, error) {
+	args := newCertManagerNamespaceArgs()
+	ns, err := corev1.NewNamespace(
+		ctx,
+		chartNamespace,
+		args,
+	)
+	if err != nil {
+		return nil, err
+	}
+	return ns, nil
+}
+
+// newCertManagerNamespaceArgs creates corev1.NamespaceArgs required for the creation of the cert-manager namespace.
+func newCertManagerNamespaceArgs() *corev1.NamespaceArgs {
+	return &corev1.NamespaceArgs{
+		Metadata: &metav1.ObjectMetaArgs{
+			Name: pulumi.String(chartNamespace),
+			Labels: pulumi.StringMap{
+				"app": pulumi.String(appLabel),
+			},
+		},
+	}
+}
