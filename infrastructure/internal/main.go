9057e8d9d1da77ff6287d09596a3e205cd30cb58
Jeremy Muhia <jmuhia80@gmail.com>
2025-07-06 16:54:09 -0400

    Deploy cert-manager and issuer from monorepo

--- infrastructure/internal/main.go
+++ infrastructure/internal/main.go
@@ -1,11 +1,30 @@
 package main
 
 import (
+	"github.com/fjarm/fjarm/infrastructure/internal/pkg/certmanager/certmanagerv1"
+	"github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
+
+const (
+	k8sProviderLogicalNamePrefix = "kubernetes-cluster-provider"
 )
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
+		k8sProvider, err := kubernetes.NewProvider(ctx, k8sProviderLogicalNamePrefix, &kubernetes.ProviderArgs{
+			//RenderYamlToDirectory: pulumi.String("yaml"),
+		})
+		if err != nil {
+			return err
+		}
+		_, err = certmanagerv1.DeployCertManager(
+			ctx,
+			k8sProvider,
+		)
+		if err != nil {
+			return err
+		}
 		return nil
 	})
 }
