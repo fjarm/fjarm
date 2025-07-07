99d137e2c8384186262b851dd5d17ea7ddf4c7f0
Jeremy Muhia <jmuhia80@gmail.com>
2025-07-06 20:44:28 -0400

    Delete redis startup package in favor of valkey (#66)

--- api/internal/redis/v1/pkg/startup/redis_startup_errors.go
+++
@@ -1,9 +0,0 @@
-package startup
-
-import "fmt"
-
-// ErrInvalidCommand is returned when trying to construct a command permission with invalid input.
-var ErrInvalidCommand = fmt.Errorf("invalid command")
-
-// ErrUnimplemented is returned from methods/functions that haven't been implemented yet.
-var ErrUnimplemented = fmt.Errorf("not implemented")
