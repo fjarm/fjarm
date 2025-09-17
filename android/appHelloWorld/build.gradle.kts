5897924133d027a3591fce0c7373ae29b5914d36
Jeremy Muhia <jmuhia80@gmail.com>
2025-09-17 19:47:37 -0400

    Bump target sdk to 35

--- android/appHelloWorld/build.gradle.kts
+++ android/appHelloWorld/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     defaultConfig {
         applicationId = "xyz.fjarm.helloworld"
         minSdk = 31
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
