fd3332812c028a69828b0fe399fe7e0ef842cfe7
Jeremy Muhia <jmuhia80@gmail.com>
2025-09-18 19:25:47 -0400

    Delete previous gRPC specific code in libServerTransport

--- android/libServerTransport/src/main/java/xyz/fjarm/libservertransport/ServerTransportModule.kt
+++ android/libServerTransport/src/main/java/xyz/fjarm/libservertransport/ServerTransportModule.kt
@@ -1,22 +1,5 @@
 package xyz.fjarm.libservertransport
 
-import io.grpc.ManagedChannel
-import io.grpc.ManagedChannelBuilder
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.asExecutor
-
 class ServerTransportModule {
 
-    fun provideGrpcServerTransport(
-        serverAddress: String,
-        serverPort: Int,
-    ): ManagedChannel {
-        return ManagedChannelBuilder
-            .forAddress(
-                serverAddress,
-                serverPort
-            )
-            .executor(Dispatchers.IO.asExecutor())
-            .build()
-    }
 }
