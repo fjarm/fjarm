654a38ec306932edb02fed6feb905f0037d8d617
Jeremy Muhia <jmuhia80@gmail.com>
2025-09-23 09:47:15 -0400

    Set up required parts for connect rpc protocol client

--- android/libServerTransport/src/main/java/xyz/fjarm/libservertransport/ServerTransportModule.kt
+++ android/libServerTransport/src/main/java/xyz/fjarm/libservertransport/ServerTransportModule.kt
@@ -1,5 +1,35 @@
 package xyz.fjarm.libservertransport
 
+import com.connectrpc.ProtocolClientConfig
+import com.connectrpc.SerializationStrategy
+import com.connectrpc.extensions.GoogleJavaLiteProtobufStrategy
+import com.connectrpc.protocols.NetworkProtocol
+
 class ServerTransportModule {
 
+    fun provideProtocolClientConfig(
+        host: String,
+        networkProtocol: NetworkProtocol,
+        serializationStrategy: SerializationStrategy,
+    ): ProtocolClientConfig {
+        // TODO(2025-09-23): Investigate using the request compression tools.
+        return ProtocolClientConfig(
+            host = host,
+            networkProtocol = networkProtocol,
+            serializationStrategy = serializationStrategy,
+        )
+    }
+
+    fun provideHost(): String {
+        // TODO(2025-09-23): Dagger inject an address instead of hardcoding localhost
+        return "10.0.2.2"
+    }
+
+    fun provideNetworkProtocol(): NetworkProtocol {
+        return NetworkProtocol.CONNECT
+    }
+
+    fun provideSerializationStrategy(): SerializationStrategy {
+        return GoogleJavaLiteProtobufStrategy()
+    }
 }
