43621452468dbc7b42b946038bee56d279cde90c
Jeremy Muhia <jmuhia80@gmail.com>
2026-01-21 17:01:28 -0500

    Depend on hilt core and compiler in hello world and server transport modules

--- android/libHelloWorld/build.gradle.kts
+++ android/libHelloWorld/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     id("java-library")
     alias(libs.plugins.jetbrainsKotlinJvm)
+
+    // Dagger and Hilt related plugins
+    alias(libs.plugins.com.google.devtools.ksp)
 }
 
 java {
@@ -9,8 +12,14 @@ java {
 }
 
 dependencies {
+    implementation(project(":libServerTransport"))
+
     implementation(libs.grpcAndroid)
     implementation(libs.grpcOkHttp)
     implementation(libs.fjarmGrpcSdk)
     implementation(libs.fjarmConnectSdk)
+
+    // Dagger and Hilt related deps
+    implementation(libs.com.google.dagger.hilt.core)
+    ksp(libs.com.google.dagger.hilt.compiler)
 }
--- android/libServerTransport/build.gradle.kts
+++ android/libServerTransport/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     id("java-library")
     alias(libs.plugins.jetbrainsKotlinJvm)
+
+    // Dagger and Hilt related plugins
+    alias(libs.plugins.com.google.devtools.ksp)
 }
 
 java {
@@ -14,4 +17,8 @@ dependencies {
     implementation(libs.okHttp3)
     implementation(libs.connectKotlinGoogleJavaLiteExt)
     implementation(libs.connectKotlinOkHttp)
+
+    // Dagger and Hilt related deps
+    implementation(libs.com.google.dagger.hilt.core)
+    ksp(libs.com.google.dagger.hilt.compiler)
 }
