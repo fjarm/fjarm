8ceb3666f2443941620d0ce00d8d8c3bb7fabfe5
Jeremy Muhia <jmuhia80@gmail.com>
2026-01-20 21:56:30 -0500

    Set explicit deps on lifecycle

--- android/appHelloWorld/build.gradle.kts
+++ android/appHelloWorld/build.gradle.kts
@@ -63,7 +63,10 @@ dependencies {
     implementation(libs.fjarmGrpcSdk)
 
     implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.compose)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
