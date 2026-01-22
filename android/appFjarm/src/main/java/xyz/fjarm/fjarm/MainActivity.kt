0cd9bdfe4697224f1e4436c05ea03deb1ce729c6
Jeremy Muhia <jmuhia80@gmail.com>
2026-01-21 21:17:40 -0500

    Enable edge to edge in fjarm app

--- android/appFjarm/src/main/java/xyz/fjarm/fjarm/MainActivity.kt
+++ android/appFjarm/src/main/java/xyz/fjarm/fjarm/MainActivity.kt
@@ -3,6 +3,7 @@ package xyz.fjarm.fjarm
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
@@ -16,6 +17,7 @@ import xyz.fjarm.fjarm.ui.theme.FjarmTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
             FjarmTheme {
