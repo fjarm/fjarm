20b3f2416598d9eae6d0a814686281d0c70bd528
Jeremy Muhia <jmuhia80@gmail.com>
2026-01-21 21:34:44 -0500

    Handle edge to edge in all Android apps (#80)

--- android/appFjarm/src/main/java/xyz/fjarm/fjarm/MainActivity.kt
+++ android/appFjarm/src/main/java/xyz/fjarm/fjarm/MainActivity.kt
@@ -3,8 +3,11 @@ package xyz.fjarm.fjarm
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -14,15 +17,19 @@ import xyz.fjarm.fjarm.ui.theme.FjarmTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
             FjarmTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                Scaffold { innerPadding ->
+                    Surface(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding),
+                        color = MaterialTheme.colorScheme.background
+                    ) {
+                        Greeting("Android")
+                    }
                 }
             }
         }
