3c60ef5b7632ae7939d39efe220fb22bda044203
Jeremy Muhia <jmuhia80@gmail.com>
2026-01-21 21:11:41 -0500

    Handle edge to edge display

--- android/appHelloWorld/src/main/java/xyz/fjarm/helloworld/MainActivity.kt
+++ android/appHelloWorld/src/main/java/xyz/fjarm/helloworld/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -32,15 +34,21 @@ import xyz.fjarm.helloworld.ui.theme.HelloWorldTheme
 class MainActivity: ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
             HelloWorldTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background,
-                ) {
-                    Greeting()
+                // Scaffold is a layout structure for the entire screen.
+                Scaffold { innerPadding ->
+                    // A surface container using the 'background' color from the theme
+                    Surface(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding),
+                        color = MaterialTheme.colorScheme.background,
+                    ) {
+                        Greeting()
+                    }
                 }
             }
         }
