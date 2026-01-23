661a1d9cb1bd0e301df6f906ce173cfee6c5f02c
Jeremy Muhia <jmuhia80@gmail.com>
2026-01-22 19:50:22 -0500

    Introduce more reasonable 100ms delay duration

--- api/internal/obfuscation/v1/pkg/interceptor/delay_duration.go
+++ api/internal/obfuscation/v1/pkg/interceptor/delay_duration.go
@@ -6,4 +6,7 @@ const (
 	// DelayDuration_15000ms represents a delay of 15000 milliseconds or 15 seconds. 15 seconds is the request timeout
 	// for most clients.
 	DelayDuration_15000ms DelayDuration = 15000
+
+	// DelayDuration_100ms represents a delay of 100 milliseconds or 0.1 seconds.
+	DelayDuration_100ms DelayDuration = 100
 )
