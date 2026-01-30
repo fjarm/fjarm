1d81fe5345e369022d5400ae718cb057e49e2813
Jeremy Muhia <jmuhia80@gmail.com>
2026-01-29 22:51:38 -0500

    Test happy path side effect in hello world view model

---
+++ android/appHelloWorld/src/test/java/xyz/fjarm/helloworld/HelloWorldViewModelTest.kt
@@ -0,0 +1,58 @@
+package xyz.fjarm.helloworld
+
+import build.buf.gen.fjarm.helloworld.v1.HelloWorldOutput
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import xyz.fjarm.libhelloworld.GetHelloWorldUseCase
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class HelloWorldViewModelTest {
+
+    private val testDispatcher = StandardTestDispatcher()
+
+    @Before
+    fun setUp() {
+        Dispatchers.setMain(testDispatcher)
+    }
+
+    @Test
+    fun processEvent_ButtonClicked_withSuccessUseCaseInvoke_emitsShowToastSideEffect() = runTest {
+        // Given a use case that returns valid, non empty output
+        val expectedOutput = "blah blah"
+        val fakeUseCase = object : GetHelloWorldUseCase {
+            override suspend fun invoke(): HelloWorldOutput {
+                return HelloWorldOutput.newBuilder()
+                    .setOutput(expectedOutput)
+                    .build()
+            }
+        }
+        val viewModel = HelloWorldViewModel(fakeUseCase)
+        
+        val collectedSideEffects = mutableListOf<HelloWorldSideEffect>()
+        backgroundScope.launch(UnconfinedTestDispatcher(testScheduler)) {
+            viewModel.sideEffect.collect { collectedSideEffects.add(it) }
+        }
+
+        // When a ButtonClicked event is processed by the ViewModel
+        viewModel.processEvent(HelloWorldEvent.ButtonClicked)
+        advanceUntilIdle()
+
+        // Then a ShowToast side effect is emitted with the expected output
+        val sideEffect = collectedSideEffects.firstOrNull()
+        assert(collectedSideEffects.size == 1)
+        assert(sideEffect is HelloWorldSideEffect.ShowToast)
+        assertEquals(
+            expectedOutput,
+            (sideEffect as HelloWorldSideEffect.ShowToast).message,
+        )
+    }
+}
