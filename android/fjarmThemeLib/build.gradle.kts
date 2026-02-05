1e9d3d521eff783578e3b7223fb5a446e8444a10
Jeremy Muhia <jmuhia80@gmail.com>
2026-02-04 20:21:02 -0500

    Set up skeleton android lib for fjarm theme

---
+++ android/fjarmThemeLib/build.gradle.kts
@@ -0,0 +1,56 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.composeCompiler)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+}
+
+android {
+    namespace = "xyz.fjarm.fjarmthemelib"
+    compileSdk = Config.COMPILE_SDK
+
+    defaultConfig {
+        minSdk = Config.MIN_SDK
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+    testOptions {
+        targetSdk = Config.TARGET_SDK
+    }
+
+    buildFeatures {
+        compose = true
+    }
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+    kotlin {
+        compilerOptions {
+            freeCompilerArgs = listOf(
+                "-XXLanguage:+PropertyParamAnnotationDefaultTargetMode",
+                "-XXLanguage:+WhenGuards",
+            )
+        }
+        jvmToolchain(JavaVersion.VERSION_21.majorVersion.toInt())
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(platform(libs.androidx.compose.bom))
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+}
