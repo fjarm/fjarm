df3ffde15d3cb024b8238a33e602c8af08ac3296
Jeremy Muhia <jmuhia80@gmail.com>
2026-02-16 18:46:56 -0500

    Migrate fjarm theme lib to convention plugins

--- android/fjarmThemeLib/build.gradle.kts
+++ android/fjarmThemeLib/build.gradle.kts
@@ -1,7 +1,9 @@
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.composeCompiler)
-    alias(libs.plugins.jetbrainsKotlinAndroid)
+    // Use the AndroidLibraryConventionPlugin to apply Android library configuration
+    id("convention.android.library")
+
+    // Use the AndroidLibraryComposeConventionPlugin to apply Jetpack Compose configuration
+    id("convention.android.library.compose")
 
     // Use the ComposeCompilerMetricsConventionPlugin to enable Compose Compiler Metrics
     id("convention.compose.metrics")
@@ -9,55 +11,9 @@ plugins {
 
 android {
     namespace = "xyz.fjarm.fjarmthemelib"
-    compileSdk = Config.COMPILE_SDK
-
-    defaultConfig {
-        minSdk = Config.MIN_SDK
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-    testOptions {
-        targetSdk = Config.TARGET_SDK
-    }
-    lint {
-        targetSdk = Config.TARGET_SDK
-    }
-
-    buildFeatures {
-        compose = true
-    }
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_21
-        targetCompatibility = JavaVersion.VERSION_21
-    }
-    kotlin {
-        compilerOptions {
-            freeCompilerArgs = listOf(
-                "-XXLanguage:+PropertyParamAnnotationDefaultTargetMode",
-                "-XXLanguage:+WhenGuards",
-            )
-        }
-        jvmToolchain(JavaVersion.VERSION_21.majorVersion.toInt())
-    }
 }
 
 dependencies {
-    implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.ui)
     implementation(libs.androidx.material3)
-    implementation(platform(libs.androidx.compose.bom))
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
 }
