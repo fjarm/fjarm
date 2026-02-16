75bf53c1232b6132def0116c2554c44127cbe459
Jeremy Muhia <jmuhia80@gmail.com>
2026-02-16 18:42:17 -0500

    Migrate login and sign up feat build file to use convention plugins

--- android/loginAndSignUpFeat/build.gradle.kts
+++ android/loginAndSignUpFeat/build.gradle.kts
@@ -1,11 +1,12 @@
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.composeCompiler)
-    alias(libs.plugins.jetbrainsKotlinAndroid)
+    // Use the AndroidLibraryConventionPlugin to apply Android library configuration
+    id("convention.android.library")
 
-    // Dagger and Hilt related plugins
-    alias(libs.plugins.com.google.devtools.ksp)
-    alias(libs.plugins.com.google.dagger.hilt.android)
+    // Use the AndroidHiltConventionPlugin to apply Dagger Hilt configuration
+    id("convention.android.hilt")
+
+    // Use the AndroidLibraryComposeConventionPlugin to apply Jetpack Compose configuration
+    id("convention.android.library.compose")
 
     // Use the ComposeCompilerMetricsConventionPlugin to enable Compose Compiler Metrics
     id("convention.compose.metrics")
@@ -13,65 +14,11 @@ plugins {
 
 android {
     namespace = "xyz.fjarm.loginandsignupfeat"
-    compileSdk = Config.COMPILE_SDK
-
-    defaultConfig {
-        minSdk = Config.MIN_SDK
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-    testOptions {
-        targetSdk = Config.TARGET_SDK
-    }
-    lint {
-        targetSdk = Config.TARGET_SDK
-    }
-
-    buildFeatures {
-        compose = true
-    }
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_21
-        targetCompatibility = JavaVersion.VERSION_21
-    }
-    kotlin {
-        compilerOptions {
-            freeCompilerArgs = listOf(
-                "-XXLanguage:+PropertyParamAnnotationDefaultTargetMode",
-                "-XXLanguage:+WhenGuards",
-            )
-        }
-        jvmToolchain(JavaVersion.VERSION_21.majorVersion.toInt())
-    }
 }
 
 dependencies {
-    // Dagger and Hilt deps
-    ksp(libs.com.google.dagger.hilt.android.compiler)
-    kspTest(libs.com.google.dagger.hilt.android.compiler)
-    implementation(libs.com.google.dagger.hilt.android)
-    implementation(libs.androidx.hilt.hilt.navigation.compose)
-
     implementation(project(":fjarmThemeLib"))
     implementation(project(":previewsLib"))
-    implementation(libs.androidx.core.ktx)
+
     implementation(libs.androidx.material3)
-    implementation(libs.androidx.compose.runtime)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(platform(libs.androidx.compose.bom))
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    debugImplementation(libs.androidx.ui.tooling)
 }
