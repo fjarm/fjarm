12283b833944b51dc26e123c63f29d84bdbe8240
Jeremy Muhia <jmuhia80@gmail.com>
2026-02-16 21:46:16 -0500

    Remove redundant dependecies in hello world app

--- android/helloWorldApp/build.gradle.kts
+++ android/helloWorldApp/build.gradle.kts
@@ -36,16 +36,13 @@ dependencies {
     implementation(project(":helloWorldLib"))
     implementation(libs.fjarmProtobufLiteSdk)
 
-    implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.compose)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
 
     testImplementation(libs.androidx.ui.test.junit4)
@@ -53,10 +50,5 @@ dependencies {
     testImplementation(libs.kotlinx.coroutines.test)
     testImplementation(libs.robolectric)
 
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 }
