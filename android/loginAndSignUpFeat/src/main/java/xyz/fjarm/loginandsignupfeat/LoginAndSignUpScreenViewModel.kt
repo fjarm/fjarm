1abae23a01e833b2d8aba766e7e3983b0abeccec
Jeremy Muhia <jmuhia80@gmail.com>
2026-02-18 20:15:14 -0500

    Create login and sign up feat view model

--- android/loginAndSignUpFeat/src/main/java/xyz/fjarm/loginandsignupfeat/LoginAndSignUpScreenViewModel.kt
+++ android/loginAndSignUpFeat/src/main/java/xyz/fjarm/loginandsignupfeat/LoginAndSignUpScreenViewModel.kt
@@ -1,2 +1,45 @@
 package xyz.fjarm.loginandsignupfeat
 
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+import javax.inject.Inject
+
+@HiltViewModel
+class LoginAndSignUpScreenViewModel @Inject constructor(
+): ViewModel() {
+
+    companion object {
+        private const val TITLE_LINE = "Fjarm"
+        private const val SUBTITLE_LINE = "Plan and execute workouts"
+        private const val LOGO = "android.resource://xyz.fjarm.loginandsignupfeat/fjarm_login_logo"
+        private const val SIGN_UP_BUTTON_TEXT = "Join for free"
+        private const val LOG_IN_BUTTON_TEXT = "Log in"
+    }
+
+    private val _state = MutableStateFlow<LoginAndSignUpState>(LoginAndSignUpState(
+        titleLine = TITLE_LINE,
+        subtitleLine = SUBTITLE_LINE,
+        logo = LOGO,
+        signUpButtonText = SIGN_UP_BUTTON_TEXT,
+        logInButtonText = LOG_IN_BUTTON_TEXT,
+    ))
+    val state = _state.asStateFlow()
+
+    private val _sideEffect = MutableSharedFlow<LoginAndSignUpSideEffect>()
+    val sideEffect = _sideEffect.asSharedFlow()
+
+    fun processEvent(event: LoginAndSignUpEvent) {
+        when (event) {
+            is LoginAndSignUpEvent.SignUpButtonClicked -> {
+                // TODO
+            }
+            is LoginAndSignUpEvent.LogInButtonClicked -> {
+                // TODO
+            }
+        }
+    }
+}
