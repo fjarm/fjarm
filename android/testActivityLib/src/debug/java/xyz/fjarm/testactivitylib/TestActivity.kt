efef622df28ff74aae3a04942b39327a8e3201f8
Jeremy Muhia <jmuhia80@gmail.com>
2026-02-25 10:12:12 -0500

    Set up test activity library for use in feature modules that do not have activities

---
+++ android/testActivityLib/src/debug/java/xyz/fjarm/testactivitylib/TestActivity.kt
@@ -0,0 +1,7 @@
+package xyz.fjarm.testactivitylib
+
+import androidx.activity.ComponentActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class TestActivity : ComponentActivity()
