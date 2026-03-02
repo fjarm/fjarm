f2071ded9364360a48ada4d2ea95cb7ec3f6cf70
Jeremy Muhia <jmuhia80@gmail.com>
2026-03-02 10:20:10 -0500

    Set up log in and sign up lib for nav keys

---
+++ android/loginAndSignUpLib/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    // Use the AndroidLibraryConventionPlugin to apply Android library configuration
+    id("convention.android.library")
+
+    // Use the HiltConventionPlugin to apply Dagger Hilt configuration
+    id("convention.hilt")
+}
+
+android {
+    namespace = "xyz.fjarm.loginandsignuplib"
+}
+
+dependencies {
+    implementation(libs.androidx.navigation3.navigation3.runtime)
+}
---
+++ android/loginAndSignUpLib/src/main/java/xyz/fjarm/loginandsignuplib/LoginAndSignUpNavKey.kt
@@ -0,0 +1,7 @@
+package xyz.fjarm.loginandsignuplib
+
+import androidx.navigation3.runtime.NavKey
+import kotlinx.serialization.Serializable
+
+@Serializable
+data object LoginAndSignUpNavKey : NavKey
