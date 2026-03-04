39cb46082ae46ccde0d97bd487bbe743fcab9b14
Jeremy Muhia <jmuhia80@gmail.com>
2026-03-03 20:57:39 -0500

    Remove hilt convention plugin from login and sign up feat lib

--- android/loginAndSignUpFeatLib/build.gradle.kts
+++ android/loginAndSignUpFeatLib/build.gradle.kts
@@ -1,9 +1,6 @@
 plugins {
     // Use the AndroidLibraryConventionPlugin to apply Android library configuration
     id("convention.android.library")
-
-    // Use the HiltConventionPlugin to apply Dagger Hilt configuration
-    id("convention.hilt")
 }
 
 android {
