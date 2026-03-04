6e3a7c319a83f979d9430d01dadc683759f727f3
Jeremy Muhia <jmuhia80@gmail.com>
2026-03-03 20:57:02 -0500

    Enable edge 2 edge before calling super onCreate method

--- android/fjarmApp/src/main/java/xyz/fjarm/fjarm/MainActivity.kt
+++ android/fjarmApp/src/main/java/xyz/fjarm/fjarm/MainActivity.kt
@@ -29,8 +29,8 @@ class MainActivity : ComponentActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
         setContent {
             FjarmTheme {
                 Scaffold { innerPadding ->
