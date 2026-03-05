4b8cbe45e5d995c96d77feeea11a4aa2459446b1
Jeremy Muhia <jmuhia80@gmail.com>
2026-03-04 21:57:06 -0500

    Bind navigator impl to navigator interface

--- android/navigationLib/src/main/java/xyz/fjarm/navigationlib/NavigatorModule.kt
+++ android/navigationLib/src/main/java/xyz/fjarm/navigationlib/NavigatorModule.kt
@@ -1,19 +1,16 @@
 package xyz.fjarm.navigationlib
 
+import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityRetainedComponent
 import dagger.hilt.android.scopes.ActivityRetainedScoped
-import xyz.fjarm.loginandsignupfeatlib.LoginAndSignUpNavKey
 
 @Module
 @InstallIn(ActivityRetainedComponent::class)
-class NavigatorModule {
+abstract class NavigatorModule {
 
-    @Provides
+    @Binds
     @ActivityRetainedScoped
-    fun provideNavigator(): Navigator {
-        return NavigatorImpl(LoginAndSignUpNavKey)
-    }
+    abstract fun bindsNavigator(impl: NavigatorImpl): Navigator
 }
