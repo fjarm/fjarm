49f524750665727e18c8ad1b901df1c2660619ff
Jeremy Muhia <jmuhia80@gmail.com>
2026-03-21 19:55:47 -0400

    start significant refactor of android app to fit better into monorepo structure

--- android/fjarmApp/src/main/java/xyz/fjarm/fjarm/FjarmApplication.kt
+++
@@ -1,8 +0,0 @@
-package xyz.fjarm.fjarm
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class FjarmApplication : Application() {
-}
--- android/testActivityLib/src/debug/java/xyz/fjarm/testactivitylib/TestActivity.kt
+++
@@ -1,7 +0,0 @@
-package xyz.fjarm.testactivitylib
-
-import androidx.activity.ComponentActivity
-import dagger.hilt.android.AndroidEntryPoint
-
-@AndroidEntryPoint
-class TestActivity : ComponentActivity()
