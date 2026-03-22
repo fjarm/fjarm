ec9bddb08af5c483cf3a97765f7dd447753df47d
Jeremy Muhia <jmuhia80@gmail.com>
2026-03-21 21:23:40 -0400

    condense app build file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,41 +1,19 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
+    // Use the AndroidApplicationConventionPlugin to apply Android application configuration
+    id("convention.android.application")
+
     alias(libs.plugins.kotlin.compose)
 }
 
 android {
     namespace = "xyz.fjarm"
-    compileSdk {
-        version = release(36)
-    }
 
     defaultConfig {
         applicationId = "xyz.fjarm"
-        minSdk = 31
-        targetSdk = 36
         versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        versionName = "1.0.0"
     }
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
     buildFeatures {
         compose = true
     }
