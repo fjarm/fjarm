3ed3346c7aa40aa5044df60ad88876e6bf7f453e
Jeremy Muhia <jmuhia80@gmail.com>
2026-03-21 20:26:22 -0400

    depend on gradle plugins for convention plugins

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -3,3 +3,10 @@ plugins {
 }
 
 group = "xyz.fjarm.buildlogic"
+
+dependencies {
+    // Get versions from the version catalog
+    compileOnly(libs.android.gradle.plugin)
+    compileOnly(libs.kotlin.gradle.plugin)
+    compileOnly(libs.compose.compiler.gradle.plugin)
+}
