bfe769fe0630a3a583330f66f081e1e9fe915b63
Jeremy Muhia <jmuhia80@gmail.com>
2026-03-22 17:43:24 -0400

    connect start destination to log in and sign up screen

--- android/app/fjarm/build.gradle.kts
+++ android/app/fjarm/build.gradle.kts
@@ -27,19 +27,14 @@ android {
 }
 
 dependencies {
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(project(":android:feature:loginAndSignUp:api"))
+    implementation(project(":android:feature:loginAndSignUp:impl"))
+
+    implementation(project(":android:library:fjarmTheme"))
+    implementation(project(":android:library:navigation"))
+
     implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
-    debugImplementation(libs.androidx.compose.ui.tooling)
-    debugImplementation(libs.androidx.compose.ui.test.manifest)
+    implementation(libs.androidx.navigation3.navigation3.runtime)
+    implementation(libs.androidx.navigation3.navigation3.ui)
 }
--- android/app/fjarm/src/main/java/xyz/fjarm/fjarm/FjarmActivity.kt
+++ android/app/fjarm/src/main/java/xyz/fjarm/fjarm/FjarmActivity.kt
@@ -7,43 +7,54 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation3.runtime.EntryProviderScope
+import androidx.navigation3.runtime.NavKey
+import androidx.navigation3.runtime.entryProvider
+import androidx.navigation3.ui.NavDisplay
 import dagger.hilt.android.AndroidEntryPoint
-import xyz.fjarm.fjarm.ui.theme.FjarmTheme
+import xyz.fjarm.fjarmtheme.FjarmTheme
+import xyz.fjarm.navigation.Navigation
+import xyz.fjarm.navigation.NavigationSideEffect
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class FjarmActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var navigator: Navigation
+
+    @Inject
+    lateinit var entryBuilders: Set<@JvmSuppressWildcards EntryProviderScope<NavKey>.() -> Unit>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             FjarmTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                ) { innerPadding ->
+                    Surface(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding),
+                    ) {
+                        NavDisplay(
+                            backStack = navigator.getBackStack(),
+                            onBack = {
+                                navigator.processSideEffect(
+                                    NavigationSideEffect.NavigateBack,
+                                )
+                            },
+                            entryProvider = entryProvider {
+                                entryBuilders.forEach { it() }
+                            }
+                        )
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FjarmTheme {
-        Greeting("Android")
     }
 }
