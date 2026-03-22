3ae36b8f8abfbde27ef845df3af72ebf0a1d7c51
Jeremy Muhia <jmuhia80@gmail.com>
2026-03-21 20:32:11 -0400

    create shared config in build-logic

---
+++ build-logic/convention/src/main/kotlin/KotlinConfig.kt
@@ -0,0 +1,27 @@
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.dsl.KotlinAndroidProjectExtension
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+internal object KotlinConfig {
+    /**
+     * Configures Kotlin for Android projects.
+     */
+    fun Project.configureKotlin() {
+        // Configure Kotlin extension
+        extensions.configure<KotlinAndroidProjectExtension> {
+            jvmToolchain(21)
+        }
+
+        // Configure Kotlin compile tasks
+        tasks.withType<KotlinCompile>().configureEach {
+            compilerOptions {
+                freeCompilerArgs.addAll(
+                    "-XXLanguage:+PropertyParamAnnotationDefaultTargetMode",
+                    "-XXLanguage:+WhenGuards",
+                )
+            }
+        }
+    }
+}
