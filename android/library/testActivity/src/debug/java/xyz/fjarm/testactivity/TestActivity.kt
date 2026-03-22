88e713ea474085b0b6340bec054f3487e00ba6ad
Jeremy Muhia <jmuhia80@gmail.com>
2026-03-22 12:47:11 -0400

    set up test activity in debug source sets

---
+++ android/library/testActivity/src/debug/java/xyz/fjarm/testactivity/TestActivity.kt
@@ -0,0 +1,7 @@
+package xyz.fjarm.testactivity
+
+import androidx.activity.ComponentActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class TestActivity : ComponentActivity()
