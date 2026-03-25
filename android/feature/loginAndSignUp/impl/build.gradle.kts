6c6dee33f0bda9af5689fb0b7f8effdec339fef7
Jeremy Muhia <jmuhia80@gmail.com>
2026-03-24 21:47:34 -0400

    use login nav key from login and sign up screen

--- android/feature/loginAndSignUp/impl/build.gradle.kts
+++ android/feature/loginAndSignUp/impl/build.gradle.kts
@@ -17,10 +17,13 @@ android {
 }
 
 dependencies {
+    implementation(project(":android:feature:login:api"))
     implementation(project(":android:feature:loginAndSignUp:api"))
+
     implementation(project(":android:library:fjarmTheme"))
     implementation(project(":android:library:navigation"))
     implementation(project(":android:library:previews"))
+
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.hilt.hilt.navigation.compose)
     implementation(libs.androidx.navigation3.navigation3.runtime)
--- android/feature/loginAndSignUp/impl/src/main/java/xyz/fjarm/loginandsignupimpl/LoginAndSignUpEntryBuilder.kt
+++ android/feature/loginAndSignUp/impl/src/main/java/xyz/fjarm/loginandsignupimpl/LoginAndSignUpEntryBuilder.kt
@@ -3,17 +3,18 @@ package xyz.fjarm.loginandsignupimpl
 import androidx.navigation3.runtime.EntryProviderScope
 import androidx.navigation3.runtime.NavKey
 import xyz.fjarm.loginandsignupapi.LoginAndSignUpNavKey
+import xyz.fjarm.loginapi.LoginNavKey
 import xyz.fjarm.navigation.Navigation
 import xyz.fjarm.navigation.NavigationSideEffect
 
 fun EntryProviderScope<NavKey>.loginAndSignUpEntryBuilder(navigation: Navigation) {
     entry<LoginAndSignUpNavKey> {
         LoginAndSignUpScreen(
-            navigateToSignUp = { navigation.processSideEffect(
-                NavigationSideEffect.NavigateToDestination())
-            },
             navigateToLogIn = { navigation.processSideEffect(
-                NavigationSideEffect.NavigateToDestination())
+                NavigationSideEffect.NavigateToDestination(LoginNavKey))
+            },
+            navigateToSignUp = {
+                // TODO(2026-03-24): navigate to sign up screen
             }
         )
     }
