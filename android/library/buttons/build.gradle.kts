c2c39b2c55dae8be09160ab60031929841e7ca0b
Jeremy Muhia <jmuhia80@gmail.com>
2026-03-29 17:33:09 -0400

    roborazzi test buttons component library

--- android/library/buttons/build.gradle.kts
+++ android/library/buttons/build.gradle.kts
@@ -2,11 +2,17 @@ plugins {
     // Use the AndroidLibraryConventionPlugin to apply Android library configuration
     id("convention.android.library")
 
+    // Use the HiltConventionPlugin to apply Dagger Hilt configuration
+    id("convention.hilt")
+
     // Use the ComposeConventionPlugin to apply Jetpack Compose configuration
     id("convention.compose")
 
     // Use the ComposeCompilerMetricsConventionPlugin to enable Compose Compiler Metrics
     id("convention.compose.metrics")
+
+    // Use the RoborazziConventionPlugin to enable Roborazzi tests
+    id("convention.roborazzi")
 }
 
 android {
@@ -18,4 +24,9 @@ dependencies {
 
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.material3)
+
+    debugImplementation(project(":android:library:testActivity"))
+
+    testDebugImplementation(libs.org.robolectric.robolectric)
+    testDebugImplementation(libs.kotlinx.coroutines.test)
 }
