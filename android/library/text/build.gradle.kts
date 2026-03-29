af486965b29dc3e5150a407d9735a81f1a583ab8
Jeremy Muhia <jmuhia80@gmail.com>
2026-03-29 17:53:57 -0400

    set up text ui component library

---
+++ android/library/text/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    // Use the AndroidLibraryConventionPlugin to apply Android library configuration
+    id("convention.android.library")
+
+    // Use the HiltConventionPlugin to apply Dagger Hilt configuration
+    id("convention.hilt")
+
+    // Use the ComposeConventionPlugin to apply Jetpack Compose configuration
+    id("convention.compose")
+
+    // Use the ComposeCompilerMetricsConventionPlugin to enable Compose Compiler Metrics
+    id("convention.compose.metrics")
+
+    // Use the RoborazziConventionPlugin to enable Roborazzi tests
+    id("convention.roborazzi")
+}
+
+android {
+    namespace = "xyz.fjarm.text"
+}
+
+dependencies {
+    implementation(project(":android:library:fjarmTheme"))
+
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.material3)
+
+    debugImplementation(project(":android:library:testActivity"))
+
+    testDebugImplementation(libs.org.robolectric.robolectric)
+    testDebugImplementation(libs.kotlinx.coroutines.test)
+}
