6ee8168e437e18d65f42b54401044f784e6cbe16
Jeremy Muhia <jmuhia80@gmail.com>
2026-04-08 20:58:08 -0400

    set up login feature impl

---
+++ android/feature/login/impl/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    // Use the AndroidLibraryConventionPlugin to apply Android library configuration
+    id("convention.android.library")
+
+    // Use the HiltConventionPlugin to apply Dagger Hilt configuration
+    id("convention.hilt")
+
+    // Use the ComposeConventionPlugin to apply Jetpack Compose configuration
+    id("convention.compose")
+
+    // Use the ComposeCompilerMetricsConventionPlugin to enable Compose Compiler Metrics
+    id("convention.compose.metrics")
+
+    // Use the RoborazziConventionPlugin to enable Roborazzi tests
+    id("convention.roborazzi")
+}
+
+android {
+    namespace = "xyz.fjarm.loginimpl"
+}
+
+dependencies {
+}
