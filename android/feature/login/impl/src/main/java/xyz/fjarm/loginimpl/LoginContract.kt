a7e132b59c9bc840d07870cbf399095bcd4829eb
Jeremy Muhia <jmuhia80@gmail.com>
2026-04-30 21:21:11 -0400

    set up basic mvi components for login screen

--- android/feature/login/impl/src/main/java/xyz/fjarm/loginimpl/LoginContract.kt
+++ android/feature/login/impl/src/main/java/xyz/fjarm/loginimpl/LoginContract.kt
@@ -1,2 +1,62 @@
 package xyz.fjarm.loginimpl
 
+import androidx.annotation.ColorRes
+import androidx.annotation.StringRes
+
+data class LoginState(
+    val header: Header,
+    val userInput: UserInput,
+    val loginButton: LoginButton,
+    val loadingIndicator: LoadingIndicator,
+    val footer: Footer,
+) {
+
+    data class Header(
+        @StringRes val headerText: Int,
+    )
+
+    data class UserInput(
+        @StringRes val emailInputLabelText: Int,
+        @ColorRes val emailInputValidityColor: Int,
+        @StringRes val passwordInputLabelText: Int,
+    )
+
+    data class LoginButton(
+        @StringRes val loginButtonText: Int,
+        val loginButtonEnabled: Boolean,
+    )
+
+    data class LoadingIndicator(
+        val loadingIndicatorVisible: Boolean,
+    )
+
+    data class Footer(
+        @StringRes val alternativeOptionsText: Int,
+        @StringRes val newToFjarmPromptText: Int,
+        @StringRes val navigateToSignUpButtonText: Int,
+        @StringRes val privacyPolicyText: Int,
+        @StringRes val termsOfServiceText: Int,
+    )
+}
+
+sealed interface LoginEvent {
+
+    data class EmailAddressModified(
+        val emailAddress: String,
+    ): LoginEvent
+
+    data class PasswordModified(
+        val password: String,
+    ): LoginEvent
+
+    data object LoginButtonClicked: LoginEvent
+}
+
+sealed interface LoginSideEffect {
+
+    data object NavigateToHome: LoginSideEffect
+
+    data class ShowSnackbar(
+        val message: String,
+    ): LoginSideEffect
+}
