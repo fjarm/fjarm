ab8e441fdc0c5049d6948606e36ecb202d994742
Jeremy Muhia <jmuhia80@gmail.com>
2026-05-03 15:12:56 -0400

    update login screen to process email and password input text

--- android/feature/login/impl/src/main/java/xyz/fjarm/loginimpl/LoginScreen.kt
+++ android/feature/login/impl/src/main/java/xyz/fjarm/loginimpl/LoginScreen.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.rememberScrollState
@@ -21,18 +20,32 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.PreviewFontScale
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.tooling.preview.PreviewScreenSizes
 import androidx.compose.ui.unit.dp
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.repeatOnLifecycle
 import xyz.fjarm.buttons.FjarmButton
 import xyz.fjarm.buttons.FjarmFilledButton
 import xyz.fjarm.fjarmtheme.FjarmTheme
@@ -43,16 +56,70 @@ import xyz.fjarm.text.FjarmSmallSizeText
 @Composable
 fun LoginScreen(
     modifier: Modifier = Modifier,
+    viewModel: LoginViewModel = hiltViewModel(),
     navigateToHome: () -> Unit = {},
 ) {
-    LoginScreenContent(
-        modifier = modifier,
-    )
+    val lifecycleOwner = LocalLifecycleOwner.current
+    val snackbarHostState = remember { SnackbarHostState() }
+
+    val sideEffects = viewModel.sideEffect
+    LaunchedEffect(sideEffects) {
+        lifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+            sideEffects.collect { sideEffect ->
+                when (sideEffect) {
+                    is LoginSideEffect.NavigateToHome -> {
+                        navigateToHome()
+                    }
+                    is LoginSideEffect.ShowSnackbar -> {
+                        snackbarHostState.showSnackbar(
+                            message = sideEffect.message,
+                            duration = SnackbarDuration.Indefinite,
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    val state by viewModel.state.collectAsStateWithLifecycle()
+
+    Scaffold(
+        containerColor = Color.Transparent,
+        snackbarHost = {
+            SnackbarHost(hostState = snackbarHostState)
+        }
+    ) { contentPadding ->
+        LoginScreenContent(
+            modifier = modifier.padding(contentPadding),
+            emailInputText = state.userInput.emailInputText,
+            onEmailInputTextModified = {
+                viewModel.processEvent(LoginEvent.EmailAddressModified(it))
+            },
+            passwordInputText = state.userInput.passwordInputText,
+            onPasswordInputTextModified = {
+                viewModel.processEvent(LoginEvent.PasswordModified(it))
+            },
+        )
+    }
 }
 
 @Composable
 private fun LoginScreenContent(
     modifier: Modifier = Modifier,
+    headerText: String = "Login",
+    emailInputText: String = "",
+    emailInputLabelText: String = "Email address",
+    passwordInputText: String = "",
+    passwordInputLabelText: String = "Password",
+    loginButtonText: String = "Continue",
+    loginButtonEnabled: Boolean = true,
+    alternativeOptionsText: String = "Or",
+    newToFjarmPromptText: String = "New to Fjarm?",
+    navigateToSignUpButtonText: String = "Sign up",
+    privacyPolicyText: String = "Privacy policy",
+    termsOfServiceText: String = "Terms of service",
+    onEmailInputTextModified: (String) -> Unit = {},
+    onPasswordInputTextModified: (String) -> Unit = {},
 ) {
     val scrollState = rememberScrollState()
 
@@ -62,23 +129,20 @@ private fun LoginScreenContent(
                 .fillMaxSize(),
         ) {
             Column(
+                // No need to apply systemBarsPadding as the wrapping Scaffold already includes it.
                 modifier = Modifier
                     .fillMaxSize()
-                    .systemBarsPadding()
                     .verticalScroll(scrollState)
-                    .padding(horizontal = 32.dp, vertical = 16.dp),
+                    .padding(horizontal = 16.dp, vertical = 16.dp),
                 horizontalAlignment = Alignment.CenterHorizontally,
             ) {
-
-                Spacer(modifier = Modifier.height(80.dp))
-
                 // Large Header
                 Column(
                     modifier = Modifier
                         .fillMaxWidth(),
                 ) {
                     FjarmExtraLargeHeaderText(
-                        text = "Login",
+                        text = headerText,
                     )
                     Box(
                         modifier = Modifier
@@ -92,14 +156,14 @@ private fun LoginScreenContent(
 
                 // Input Field Section
                 OutlinedTextField(
-                    value = "",
-                    onValueChange = { },
+                    value = emailInputText,
+                    onValueChange = { onEmailInputTextModified(it) },
                     modifier = Modifier
                         .fillMaxWidth()
                         .defaultMinSize(minHeight = 56.dp),
                     label = {
                         FjarmNormalSizeText(
-                            text = "Email address",
+                            text = emailInputLabelText,
                             fontWeight = FontWeight.Bold,
                         )
                     },
@@ -112,17 +176,20 @@ private fun LoginScreenContent(
                 )
 
                 OutlinedTextField(
-                    value = "",
-                    onValueChange = { },
+                    value = passwordInputText,
+                    onValueChange = {
+                        onPasswordInputTextModified(it)
+                    },
                     modifier = Modifier
                         .fillMaxWidth()
                         .defaultMinSize(minHeight = 56.dp),
                     label = {
                         FjarmNormalSizeText(
-                            text = "Password",
+                            text = passwordInputLabelText,
                             fontWeight = FontWeight.Bold,
                         )
                     },
+                    visualTransformation = PasswordVisualTransformation(),
                     shape = RoundedCornerShape(4.dp),
                     keyboardOptions = KeyboardOptions(
                         keyboardType = KeyboardType.Password,
@@ -139,9 +206,10 @@ private fun LoginScreenContent(
                     modifier = Modifier
                         .fillMaxWidth()
                         .defaultMinSize(minHeight = 56.dp),
+                    enabled = loginButtonEnabled,
                 ) {
                     FjarmNormalSizeText(
-                        text = "Continue",
+                        text = loginButtonText,
                         fontWeight = FontWeight.Black,
                     )
                 }
@@ -157,7 +225,7 @@ private fun LoginScreenContent(
                         modifier = Modifier.weight(1f),
                     )
                     FjarmNormalSizeText(
-                        text = "Or",
+                        text = alternativeOptionsText,
                         modifier = Modifier.padding(horizontal = 16.dp),
                         fontWeight = FontWeight.Bold
                     )
@@ -170,7 +238,7 @@ private fun LoginScreenContent(
 
                 // Footer Links
                 FjarmNormalSizeText(
-                    text = "New to Fjarm?",
+                    text = newToFjarmPromptText,
                     fontWeight = FontWeight.Bold,
                 )
                 FjarmButton(
@@ -179,7 +247,7 @@ private fun LoginScreenContent(
                     onClick = { },
                 ) {
                     FjarmNormalSizeText(
-                        text = "Sign up",
+                        text = navigateToSignUpButtonText,
                         fontWeight = FontWeight.Bold,
                         textDecoration = TextDecoration.Underline,
                     )
@@ -193,8 +261,8 @@ private fun LoginScreenContent(
                         .fillMaxWidth(),
                     horizontalArrangement = Arrangement.SpaceEvenly,
                 ) {
-                    FjarmSmallSizeText("Privacy policy")
-                    FjarmSmallSizeText("Terms of service")
+                    FjarmSmallSizeText(privacyPolicyText)
+                    FjarmSmallSizeText(termsOfServiceText)
                 }
             }
         }
