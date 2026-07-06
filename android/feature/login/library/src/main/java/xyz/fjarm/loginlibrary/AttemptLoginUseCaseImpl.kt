2db64c9573ea84919c1caddfb3bdff33d6ba81b1
Jeremy Muhia <jmuhia80@gmail.com>
2026-07-06 09:55:16 -0400

    use login repository in attempt login use case

--- android/feature/login/library/src/main/java/xyz/fjarm/loginlibrary/AttemptLoginUseCaseImpl.kt
+++ android/feature/login/library/src/main/java/xyz/fjarm/loginlibrary/AttemptLoginUseCaseImpl.kt
@@ -1,13 +1,45 @@
 package xyz.fjarm.loginlibrary
 
+import com.connectrpc.Code
+import com.connectrpc.ConnectException
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class AttemptLoginUseCaseImpl @Inject constructor(): AttemptLoginUseCase {
+class AttemptLoginUseCaseImpl @Inject constructor(
+    private val loginRepository: LoginRepository,
+): AttemptLoginUseCase {
+
+    companion object {
+        private const val ERROR_GREETING = "RUH-ROH!"
+        private const val HIDDEN_ERROR = "Something went wrong. Try again later."
+        private const val UNAVAILABLE_MESSAGE = "Something went wrong on our end. Try again later."
+    }
 
     override suspend fun invoke(email: String, password: String): Result<Unit> {
-        // TODO(2026-05-15): Update this with real logic.
-        return Result.success(Unit)
+        // Use cases are responsible for being thread safe.
+        // SEE: https://developer.android.com/kotlin/coroutines/coroutines-best-practices#main-safe
+        return withContext(Dispatchers.IO) {
+            try {
+                // TODO(2026-07-06): Implement saving the session tokens to an encrypted DataStore.
+                loginRepository.createSession(email, password)
+                Result.success(Unit)
+            } catch (e: ConnectException) {
+                val message = when (e.code) {
+                    Code.UNAVAILABLE -> UNAVAILABLE_MESSAGE
+                    else -> {
+                        // In a production app, consider logging this unexpected exception.
+                        HIDDEN_ERROR
+                    }
+                }
+                Result.failure(Throwable(message))
+            } catch (_: Exception) {
+                // Generic error handling is reserved for truly exceptional errors that happen
+                // outside ConnectRPC.
+                Result.failure(Throwable("$ERROR_GREETING $HIDDEN_ERROR"))
+            }
+        }
     }
 }
