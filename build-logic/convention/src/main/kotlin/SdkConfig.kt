431c21e69c57ca1d6a82a3eccde373880283e6dd
Jeremy Muhia <jmuhia80@gmail.com>
2026-07-06 10:07:42 -0400

    upgrade target sdk to 37

--- build-logic/convention/src/main/kotlin/SdkConfig.kt
+++ build-logic/convention/src/main/kotlin/SdkConfig.kt
@@ -1,4 +1,7 @@
+import org.gradle.api.JavaVersion
+
 internal const val minSDK = 31
-internal const val targetSDK = 36
+internal const val targetSDK = 37
 
 internal const val jDKVersion = 21
+internal val javaVersion = JavaVersion.VERSION_21
