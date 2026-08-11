2d1d3e571172ad199b9ae950125d3ae3ced34fd2
Jeremy Muhia <jmuhia80@gmail.com>
2026-08-10 22:03:12 -0400

    supply idempotency key to create session request

--- android/feature/login/library/src/main/java/xyz/fjarm/loginlibrary/LoginConnectRepositoryImpl.kt
+++ android/feature/login/library/src/main/java/xyz/fjarm/loginlibrary/LoginConnectRepositoryImpl.kt
@@ -6,6 +6,7 @@ import build.buf.gen.fjarm.authentication.v1.Session
 import build.buf.gen.fjarm.users.v1.UserEmailAddress
 import build.buf.gen.fjarm.users.v1.UserPassword
 import com.connectrpc.getOrThrow
+import java.util.UUID
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -18,11 +19,16 @@ class LoginConnectRepositoryImpl @Inject constructor(
         email: String,
         password: String,
     ): Session {
+        val idempotencyKey = UUID.randomUUID().toString()
         val request = CreateSessionRequest.newBuilder()
+            .setIdempotencyKey(idempotencyKey)
             .setEmailAddress(UserEmailAddress.newBuilder().setEmailAddress(email).build())
             .setPassword(UserPassword.newBuilder().setPassword(password).build())
             .build()
-        val response = client.createSession(request, headers = emptyMap()).getOrThrow()
+        val response = client.createSession(
+            request,
+            headers = mapOf("idempotency_key" to listOf(idempotencyKey)),
+        ).getOrThrow()
         return response.session
     }
 }
