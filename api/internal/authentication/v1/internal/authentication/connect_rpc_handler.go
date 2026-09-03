42d68db10e68ad1ac3a136235e92dadfb86ecf9d
Jeremy Muhia <jmuhia80@gmail.com>
2026-09-03 01:41:49 -0400

    set up connect handler constructor method

--- api/internal/authentication/v1/internal/authentication/connect_rpc_handler.go
+++ api/internal/authentication/v1/internal/authentication/connect_rpc_handler.go
@@ -4,11 +4,41 @@ import (
 	"log/slog"
 
 	"buf.build/gen/go/fjarm/fjarm/connectrpc/go/fjarm/authentication/v1/authenticationv1connect"
+	authenticationpb "buf.build/gen/go/fjarm/fjarm/protocolbuffers/go/fjarm/authentication/v1"
 	"buf.build/go/protovalidate"
+
+	"github.com/fjarm/fjarm/api/internal/logkeys"
 )
+
+const connectRPCHandlerTag = "connect_rpc_handler"
 
 type AuthenticationServiceConnectRPCHandler struct {
 	authenticationv1connect.UnimplementedAuthenticationServiceHandler
 	logger    *slog.Logger
 	validator protovalidate.Validator
+}
+
+func NewConnectRPCHandler(l *slog.Logger) *AuthenticationServiceConnectRPCHandler {
+	logger := l.With(
+		slog.String(logkeys.Tag, connectRPCHandlerTag),
+	)
+
+	validator, err := protovalidate.New(
+		protovalidate.WithDisableLazy(),
+		protovalidate.WithFailFast(),
+		protovalidate.WithMessages(
+			&authenticationpb.CreateSessionRequest{},
+			&authenticationpb.CreateSessionResponse{},
+		),
+	)
+	if err != nil {
+		logger.Error("failed to create message validator", slog.Any(logkeys.Err, err))
+		return nil
+	}
+
+	h := AuthenticationServiceConnectRPCHandler{
+		logger:    logger,
+		validator: validator,
+	}
+	return &h
 }
