48fd6cf6cdf6b6f0e5416b813e4a00f7628974b7
Jeremy Muhia <jmuhia80@gmail.com>
2026-09-03 01:55:17 -0400

    set up main func for authentication service

---
+++ api/internal/authentication/v1/cmd/authentication/main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log/slog"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+
+	"buf.build/gen/go/fjarm/fjarm/connectrpc/gosimple/fjarm/authentication/v1/authenticationv1connect"
+	"connectrpc.com/connect"
+
+	"github.com/fjarm/fjarm/api/internal/authentication/v1/internal/authentication"
+	"github.com/fjarm/fjarm/api/internal/logkeys"
+	obfuscation "github.com/fjarm/fjarm/api/internal/obfuscation/v1/pkg/interceptor"
+	tracing "github.com/fjarm/fjarm/api/internal/tracing/v1/pkg/interceptor"
+)
+
+const ip = "[::]"
+
+const mainTag = "main"
+
+func main() {
+	logger := slog.New(
+		slog.NewJSONHandler(os.Stdout, nil),
+	).With(
+		slog.String(logkeys.Service, authenticationv1connect.AuthenticationServiceName),
+	)
+
+	// Handle SIGINT (CTRL+C) gracefully.
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		logger.ErrorContext(ctx, "failed to read port from environment", slog.String(logkeys.Tag, mainTag))
+		os.Exit(1)
+	}
+	addr := fmt.Sprintf("%s:%s", ip, port)
+
+	interceptors := connect.WithInterceptors(
+		obfuscation.NewConnectRPCConstantTimingInterceptor(logger, obfuscation.DelayDuration_100ms),
+		tracing.NewConnectRPCRequestIDLoggingInterceptor(logger),
+	)
+	connectRPCHandler := authentication.NewConnectRPCHandler(logger)
+	path, handler := authenticationv1connect.NewAuthenticationServiceHandler(connectRPCHandler, interceptors)
+
+	mux := http.NewServeMux()
+	mux.Handle(path, handler)
+
+	srv := &http.Server{
+		Addr: addr,
+		BaseContext: func(_ net.Listener) context.Context {
+			return ctx
+		},
+		Handler: mux,
+	}
+
+	defer func() {
+		logger.InfoContext(ctx, "shut down server", slog.String(logkeys.Tag, mainTag))
+
+		err := srv.Shutdown(ctx)
+		if err != nil && !errors.Is(err, http.ErrServerClosed) {
+			logger.ErrorContext(ctx, "failed to shut down server", slog.String(logkeys.Tag, mainTag))
+		}
+	}()
+
+	srvErrChan := make(chan error, 1)
+	go func() {
+		logger.InfoContext(ctx, "started server", slog.String(logkeys.Tag, mainTag))
+		srvErrChan <- srv.ListenAndServe()
+	}()
+
+	// Wait for interruption.
+	select {
+	case err := <-srvErrChan:
+		// Error when starting HTTP server.
+		logger.ErrorContext(
+			ctx,
+			"stopping server after error",
+			slog.String(logkeys.Tag, mainTag),
+			slog.Any(logkeys.Err, err),
+		)
+		return
+	case <-ctx.Done():
+		// Wait for first CTRL+C.
+		// Stop receiving signal notifications as soon as possible.
+		stop()
+	}
+}
