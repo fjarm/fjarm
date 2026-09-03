094671c7b9e03e8b86a1ce9f43d19c0383a12f2b
Jeremy Muhia <jmuhia80@gmail.com>
2026-09-02 22:52:09 -0400

    stub unimplemented create session method

--- api/internal/authentication/v1/internal/authentication/connect_rpc_handler.go
+++ api/internal/authentication/v1/internal/authentication/connect_rpc_handler.go
@@ -3,10 +3,12 @@ package authentication
 import (
 	"log/slog"
 
+	"buf.build/gen/go/fjarm/fjarm/connectrpc/go/fjarm/authentication/v1/authenticationv1connect"
 	"buf.build/go/protovalidate"
 )
 
 type AuthenticationServiceConnectRPCHandler struct {
+	authenticationv1connect.UnimplementedAuthenticationServiceHandler
 	logger    *slog.Logger
 	validator protovalidate.Validator
 }
--- api/internal/authentication/v1/internal/authentication/connect_rpc_handler_create_session.go
+++ api/internal/authentication/v1/internal/authentication/connect_rpc_handler_create_session.go
@@ -1,1 +1,15 @@
 package authentication
+
+import (
+	"context"
+
+	authenticationpb "buf.build/gen/go/fjarm/fjarm/protocolbuffers/go/fjarm/authentication/v1"
+	"connectrpc.com/connect"
+)
+
+func (h *AuthenticationServiceConnectRPCHandler) CreateSession(
+	ctx context.Context,
+	req *connect.Request[authenticationpb.CreateSessionRequest],
+) (*connect.Response[authenticationpb.CreateSessionResponse], error) {
+	return h.UnimplementedAuthenticationServiceHandler.CreateSession(ctx, req)
+}
