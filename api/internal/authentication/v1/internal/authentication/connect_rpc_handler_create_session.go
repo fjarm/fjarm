36a862fcf02f45898f605f07619bf1c0ffd8ee6f
Jeremy Muhia <jmuhia80@gmail.com>
2026-09-03 01:34:13 -0400

    use gosimple version of connectrpc in authentication service

--- api/internal/authentication/v1/internal/authentication/connect_rpc_handler_create_session.go
+++ api/internal/authentication/v1/internal/authentication/connect_rpc_handler_create_session.go
@@ -4,12 +4,11 @@ import (
 	"context"
 
 	authenticationpb "buf.build/gen/go/fjarm/fjarm/protocolbuffers/go/fjarm/authentication/v1"
-	"connectrpc.com/connect"
 )
 
 func (h *AuthenticationServiceConnectRPCHandler) CreateSession(
 	ctx context.Context,
-	req *connect.Request[authenticationpb.CreateSessionRequest],
-) (*connect.Response[authenticationpb.CreateSessionResponse], error) {
-	return h.UnimplementedAuthenticationServiceHandler.CreateSession(ctx, req)
+	req *authenticationpb.CreateSessionRequest,
+) (*authenticationpb.CreateSessionResponse, error) {
+	return nil, nil
 }
