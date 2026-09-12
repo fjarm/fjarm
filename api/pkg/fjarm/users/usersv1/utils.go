75e2eef2028700d9b2b593eacb386ddbeeddfd6e
Jeremy Muhia <jmuhia80@gmail.com>
2026-09-12 00:34:56 -0400

    remove presence check and context param from user message validation helpers

--- api/pkg/fjarm/users/usersv1/utils.go
+++ api/pkg/fjarm/users/usersv1/utils.go
@@ -1,54 +1,33 @@
 package usersv1
 
 import (
-	"context"
-	"strings"
-
 	userspb "buf.build/gen/go/fjarm/fjarm/protocolbuffers/go/fjarm/users/v1"
 	"buf.build/go/protovalidate"
 )
 
-func ValidateUserEmailAddress(_ context.Context, email *userspb.UserEmailAddress) error {
+func ValidateUserEmailAddress(email *userspb.UserEmailAddress) error {
 	if email == nil {
-		return ErrValidationError
-	}
-	if !email.HasEmailAddress() {
 		return ErrValidationError
 	}
 	return protovalidate.Validate(email)
 }
 
-func ValidateUserHandle(_ context.Context, handle *userspb.UserHandle) error {
+func ValidateUserHandle(handle *userspb.UserHandle) error {
 	if handle == nil {
-		return ErrValidationError
-	}
-	if !handle.HasHandle() {
-		return ErrValidationError
-	}
-	if strings.Contains(handle.GetHandle(), " ") {
-		return ErrValidationError
-	}
-	if handle.GetHandle() == "" {
 		return ErrValidationError
 	}
 	return protovalidate.Validate(handle)
 }
 
-func ValidateUserID(_ context.Context, id *userspb.UserId) error {
+func ValidateUserID(id *userspb.UserId) error {
 	if id == nil {
-		return ErrValidationError
-	}
-	if !id.HasUserId() {
 		return ErrValidationError
 	}
 	return protovalidate.Validate(id)
 }
 
-func ValidateUserPassword(_ context.Context, pwd *userspb.UserPassword) error {
+func ValidateUserPassword(pwd *userspb.UserPassword) error {
 	if pwd == nil {
-		return ErrValidationError
-	}
-	if !pwd.HasPassword() {
 		return ErrValidationError
 	}
 	return protovalidate.Validate(pwd)
